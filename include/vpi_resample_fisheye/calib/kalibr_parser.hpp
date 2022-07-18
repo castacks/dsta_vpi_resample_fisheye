8bc4dba4657c27bda0bb5b0fa93cb4ba490990c2
Duisterhof <b.p.duisterhof@student.tudelft.nl>
2022-07-18 19:42:18 -0400

    generic init

---
+++ include/vpi_resample_fisheye/calib/kalibr_parser.hpp
@@ -0,0 +1,38 @@
+
+#ifndef __KALIBR_PARSER_HPP__
+#define __KALIBR_PARSER_HPP__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <yaml-cpp/yaml.h>
+
+#include <Eigen/Dense>
+
+#include "vpi_resample_fisheye/camera_model/double_sphere.hpp"
+
+namespace mvs
+{
+
+class KalibrParser {
+public:
+    KalibrParser() = default;
+    ~KalibrParser() = default;
+
+    DoubleSphere parser_camera_node(
+        YAML::Node&& node,
+        const std::string& name,
+        const TransformMat& previous_chain_pose = TransformMat::Identity()
+    ) const ;
+
+public:
+    static const std::string KALIBR_INTRINSICS;
+    static const std::string KALIBR_EXTRINSICS;
+    static const std::string KALIBR_RESOLUTION;
+    static const std::string KALIBR_ROSTOPIC;
+};
+
+} // namespace mvs
+
+#endif // __KALIBR_PARSER_HPP__
---
+++ src/camera_model/camera_model_base.cpp
@@ -0,0 +1,75 @@
+
+#include <sstream>
+
+#include "vpi_resample_fisheye/camera_model/camera_model_base.hpp"
+
+using namespace mvs;
+
+namespace mvs
+{
+
+TransformMat inverse_transform( const TransformMat& T ) {
+    TransformMat inversed = TransformMat::Identity();
+
+    const auto RT = T.block(0, 0, 3, 3).transpose();
+
+    inversed.block(0, 0, 3, 3) = RT;
+    inversed.block(0, 3, 3, 1) = -RT * T.block(0, 3, 3, 1);
+
+    return inversed;
+}
+
+Quat ypr_2_quat(float y, float p, float r) {
+    return Eigen::AngleAxis<float>( y, Eigen::Vector3f::UnitZ() )
+         * Eigen::AngleAxis<float>( p, Eigen::Vector3f::UnitY() )
+         * Eigen::AngleAxis<float>( r, Eigen::Vector3f::UnitX() );
+}
+
+
+} // namespace mvs
+
+std::string Shape_t::string() const {
+    std::stringstream ss;
+    ss << "(h, w) " << h << ", " << w;
+    return ss.str();
+}
+
+namespace mvs
+{
+
+std::ostream& operator << ( std::ostream& os, const Shape_t& shape ) {
+    os << shape.string();
+    return os;
+}
+
+} // namespace mvs
+
+
+
+CameraModel::CameraModel(const std::string& name, float fx, float fy, float cx, float cy, const Shape_t& shape)
+: name(name), fx(fx), fy(fy), cx(cx), cy(cy), shape(shape) 
+{
+    topic_name = "";
+    extrinsics = TransformMat::Identity();
+}
+
+
+
+CameraModel::~CameraModel()
+{}
+
+
+std::string CameraModel::string() const {
+    std::stringstream ss;
+
+    ss << "name = " << name << "\n"
+       << "topic_name = " << topic_name << "\n"
+       << "fx = " << fx << "\n"
+       << "fy = " << fy << "\n"
+       << "cx = " << cx << "\n"
+       << "cy = " << cy << "\n"
+       << "shape = " << shape << "\n"
+       << "extrinsics = \n" << extrinsics;
+
+    return ss.str();
+}
